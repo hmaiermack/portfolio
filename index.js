b9c3d2cdbab951caaa14d6efedc096f346945c53
Mack Maier <hmaiermack@gmail.com>
2020-07-22 06:14:00 -0700

    new feature: fetch recent pushes from github and display them

---
+++ index.js
@@ -0,0 +1,33 @@
+let loading = true
+let filtered;
+
+async function fetchData(){
+    let response = await fetch('https://api.github.com/users/hmaiermack/events')
+
+    let data = await response.json();
+    filtered = data.filter(item => item.type === "PushEvent").slice(0, 5)
+
+    loading = false;
+}
+
+function populateDiv() {
+    loading ? $("#github-activity").append("loading...")
+    : filtered.forEach(item => {
+        $("#github-div").append(`
+        <div style="display:flex; align-items:flex-start">
+            <h3 style="margin: 0;">${item.created_at.substr(0, 10)}</h3>
+            <h4 style="margin: 0 0 0.5em 0; flex-direction: row; color: #689dca;">${item.actor.display_login} |&nbsp;
+            <a  href="${item.payload.commits[0].url}">commit url</a> &nbsp;|&nbsp;
+            <a  href="${item.repo.url}">repo url</a></h4>
+            <p style="margin-top: 0; text-align: left;">${item.payload.commits[0].message}</p>
+        </div>
+        `)
+    })
+}
+
+async function makeGithubDiv(){
+    await fetchData()
+    populateDiv()
+}
+
+makeGithubDiv()
