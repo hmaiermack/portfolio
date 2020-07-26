ea90a0f8c1c4e6fbdefa794db888d146705026bc
Mack Maier <hmaiermack@gmail.com>
2020-07-25 17:17:31 -0700

    changed repo <a> to be subheader for recent github activity

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function fetchData(){
 
     let data = await response.json();
     filtered = data.filter(item => item.type === "PushEvent").slice(0, 5)
-
+    console.log(filtered[0])
 }
 
 function populateDiv() {
@@ -14,10 +14,10 @@ function populateDiv() {
     filtered.forEach(item => {
         $("#github-div").append(`
         <div style="display:flex; align-items:flex-start">
-            <h3 style="margin: 0;">${item.created_at.substr(0, 10)}</h3>
-            <h4 style="margin: 0 0 0.5em 0; flex-direction: row; color: #689dca;">${item.actor.display_login} |&nbsp;
-            <a  href="https://github.com/${item.repo.name}/commit/${item.payload.head}">commit url</a> &nbsp;|&nbsp;
-            <a  href="https://github.com/${item.repo.name}">repo url</a></h4>
+            <h3 style="margin: 0; flex-direction: row">${item.created_at.substr(0, 10)}</h3>
+            <h3 style="margin: 0; text-decoration: none;"><a href="https://github.com/${item.repo.name}">${item.repo.name}</a></h3>
+            <h4 style="margin: 0; flex-direction: row; color: #689dca;">${item.actor.display_login} |&nbsp;
+            <a  href="https://github.com/${item.repo.name}/commit/${item.payload.head}">commit url</a></h4>
             <p style="margin-top: 0; text-align: left;">${item.payload.commits[0].message}</p>
         </div>
         `)
