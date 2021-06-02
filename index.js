864a74c594561ced1cb8a307a7764bf68734238d
Mack Maier <hmaiermack@gmail.com>
2021-06-02 10:03:26 -0700

    style changes

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function populateDiv() {
     $("#github-div").append(`<h2 style="align-self: center;">Recent Github Activity</h2>`)
     filtered.forEach(item => {
         $("#github-div").append(`
-        <div style="display:flex; align-items:flex-start">
-            <h3 style="margin: 0; flex-direction: row">${item.created_at.substr(0, 10)}</h3>
+        <div style="display:flex; align-items:flex-start; padding-bottom: 1rem;">
+            <h3 style="margin: 0; flex-direction: row"><strong>${item.created_at.substr(0, 10)}</strong></h3>
             <h3 style="margin: 0; text-decoration: none;"><a href="https://github.com/${item.repo.name}">${item.repo.name}</a></h3>
             <h4 style="margin: 0; flex-direction: row; color: #689dca;">${item.actor.display_login} |&nbsp;
             <a  href="https://github.com/${item.repo.name}/commit/${item.payload.head}">commit url</a></h4>
-            <p style="margin-top: 0; text-align: left;">${item.payload.commits[0].message}</p>
+            <p style="margin-top: 0; text-align: left; font-size: large;">${item.payload.commits[0].message}</p>
         </div>
         `)
     })
